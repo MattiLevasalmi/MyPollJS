230caaae9064057550c4fbff5ca4d512953c5358
MattiLevasalmi <mattic.levasalmi@gmail.com>
2024-03-04 13:08:40 +0200

    Added shell of login page and fixed some issues with npm

--- app/src/Login.tsx
+++ app/src/Login.tsx
@@ -1,9 +1,18 @@
 
 
 export default function Login(){
+
+    const submitLogin = () => {
+        console.log("login");
+    }
+
     return(
-        <>
-            <h1>Welcome to The Poll Whisperer</h1>
-        </>
+        <div>
+            <form onSubmit={submitLogin}>
+                <p><input type="email" id="email" required></input></p>
+                <p><input type="password" id="password" required></input></p>
+                <p><button type="submit">Login</button></p>
+            </form>
+        </div>
     )
 }
