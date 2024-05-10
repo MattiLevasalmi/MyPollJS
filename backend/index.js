dd380e54daf265c417b7dbbe99e24e4a944975e9
Matti Leväsalmi <mattic.levasalmi@gmail.com>
2024-05-10 17:48:00 +0300

    cors origin change

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(express.json());
 
 var corsOptions = {
-    origin: 'https://pollapi.vercel.app',
+    origin: 'https://pollapi.vercel.app/Login',
     optionSuccessStatus: 200
 }
 
